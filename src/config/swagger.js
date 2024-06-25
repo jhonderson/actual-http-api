ff9887293c1cf83cc3ea97a37fc6891ed926bf95
Jhon Cardenas <jhonderson2007@gmail.com>
2024-06-24 18:50:55 -0700

    Bumping version

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -16,7 +16,7 @@ const openapiSpecification = swaggerJsdoc({
           name: 'MIT',
           url: 'http://opensource.org/licenses/MIT',
         },
-        version: '0.9.0',
+        version: '0.9.1',
       },
       components: {
         securitySchemes: {
