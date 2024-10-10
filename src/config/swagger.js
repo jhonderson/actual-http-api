49f42d48787c7a0b950d4e435c1dbdcbf8c37c3a
Jhon Cardenas <jhonderson2007@gmail.com>
2024-10-09 22:39:24 -0700

    Update Actual Api Nodejs dependency version and bumping version

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -16,7 +16,7 @@ const openapiSpecification = swaggerJsdoc({
           name: 'MIT',
           url: 'http://opensource.org/licenses/MIT',
         },
-        version: '0.12.0',
+        version: '0.12.1',
       },
       components: {
         securitySchemes: {
