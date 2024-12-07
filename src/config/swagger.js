72292bb2efedac7a6a76a450f5569a2ff189dc57
Jhon Cardenas <jhonderson2007@gmail.com>
2024-12-06 23:23:21 -0800

    Updating Actual Api NodeJS dependency

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -16,7 +16,7 @@ const openapiSpecification = swaggerJsdoc({
           name: 'MIT',
           url: 'http://opensource.org/licenses/MIT',
         },
-        version: '0.14.0',
+        version: '0.15.0',
       },
       components: {
         securitySchemes: {
